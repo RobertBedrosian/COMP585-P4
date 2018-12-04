9a0e671c76f3998b81dca2676c328aa3048e7a75
cooljohnny3 <cooljohnny3@gmail.com>
2018-12-03 21:32:36 -0800

    Added dummy data to be used for testing

--- initilizedb.sql
+++ initilizedb.sql
@@ -33,3 +33,20 @@ CREATE TABLE IF NOT EXISTS friends (
               PRIMARY KEY (id),
               FOREIGN KEY (user_id1) REFERENCES users(id),
               FOREIGN KEY (user_id2) REFERENCES users(id));
+
+INSERT INTO users (first_name, last_name, email, username, age, password, salt) VALUES ("John", "Brehm", "brehmjohn3@gmail.com", "cooljohnny3", 23, 1, 1);
+INSERT INTO users (first_name, last_name, email, username, age, password, salt) VALUES ("Peter", "Brehm", "brehmpeter@gmail.com", "profduke49", 23, 1, 1);
+INSERT INTO users (first_name, last_name, email, username, age, password, salt) VALUES ("Robert", "Bedrosian", "rob.bedrosian@gmail.com", "mrconfus3d", 23, 1, 1);
+INSERT INTO users (first_name, last_name, email, username, age, password, salt) VALUES ("Armine", "Khachatryan", "armine.Khachatryan@gmail.com", "arminek", 23, 1, 1);
+INSERT INTO users (first_name, last_name, email, username, age, password, salt) VALUES ("Willy", "Alicon", "willy.Alicon@gmail.com", "willyA", 23, 1, 1);
+
+INSERT INTO friends (user_id1, user_id2) VALUES (1, 2);
+INSERT INTO friends (user_id1, user_id2) VALUES (2, 1);
+INSERT INTO friends (user_id1, user_id2) VALUES (1, 3);
+INSERT INTO friends (user_id1, user_id2) VALUES (3, 1);
+INSERT INTO friends (user_id1, user_id2) VALUES (1, 5);
+INSERT INTO friends (user_id1, user_id2) VALUES (5, 1);
+INSERT INTO friends (user_id1, user_id2) VALUES (3, 4);
+INSERT INTO friends (user_id1, user_id2) VALUES (4, 3);
+INSERT INTO friends (user_id1, user_id2) VALUES (3, 5);
+INSERT INTO friends (user_id1, user_id2) VALUES (5, 3);
